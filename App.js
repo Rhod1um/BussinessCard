5344665febc927c01f9d8366dd97f6e449144a9c
Veronica Frydkjær <frida-nanni@hotmail.com>
2023-08-21 17:55:26 +0200

    third commit

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+//^dette skal altid importeres
+//man skal manuelt fortælle hvilke react komponenter man har, her er
+//button nu tilføjet
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your!</Text>
       <StatusBar style="auto" />
+      <View style={styles.knap}>
+        <Button title='Knap' style={"backgroundColor: '#aaa'"}></Button>
+      </View>
     </View>
   );
 }
@@ -13,8 +19,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#aaa',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  knap: {
+    color: '#784534',
+    backgroundColor: '#ggg',
+  }
 });
